26abd1fd8f51fe01ae6b78b598946d30e6b0e62b
NEMK <NEMK@bang-olufsen.dk>
2021-11-23 10:57:16 +0100

    Fixed windows-related install bug

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ setup(
 
     # When your source code is in a subdirectory under the project root, e.g.
     # `src/`, it is necessary to specify the `package_dir` argument.
-    package_dir={'': 'pydetectability/'},  # Optional
+    package_dir={'': 'pydetectability'},  # Optional
 
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
@@ -117,7 +117,7 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(where='pydetectability/'),  # Required
+    packages=find_packages(where='pydetectability'),  # Required
 
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
